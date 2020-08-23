2f18191bddd7e314d3f26727d758fb1887e292f6
Utpal Singh <utpal@beecash.io>
2020-08-23 12:19:30 +0530

    Updates

---
+++ precache-manifest.7d143017ada7cf956aa22fa3399d0e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c09e08fb6695fd2f11a26926dfef84",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "0ed7f75b6be13ab073f3",
+    "url": "/keeper-app-react/static/js/2.275ec37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keeper-app-react/static/js/2.275ec37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a0fc54af1a2f44ab8a",
+    "url": "/keeper-app-react/static/js/main.a671cb25.chunk.js"
+  },
+  {
+    "revision": "82bf9eb591b886d2a991",
+    "url": "/keeper-app-react/static/js/runtime-main.1bdaf283.js"
+  }
+]);
