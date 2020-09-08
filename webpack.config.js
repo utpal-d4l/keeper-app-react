0e429c1e17bfaf4695758bc6acfdfa50593c6b60
Utpal Singh <utpal@beecash.io>
2020-09-08 23:25:32 +0530

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,55 @@
 const path = require('path')
 
 const webpack = require('webpack')
-const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const envKeys = require('./config')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    chunkFilename: '[id].js',
-    publicPath: ''
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]'
-              },
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                autoprefixer({})
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        loader: 'url-loader?limit=10000&name=img/[name].[ext]'
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin(envKeys),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/public/index.html'),
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ]
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    entry: './src/index.js',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: isProduction ? [MiniCssExtractPlugin.loader, 'css-loader'] : ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin(envKeys),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        filename: 'index.html',
+        inject: true
+      }),
+      isProduction
+      && new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      })
+    ].filter(Boolean)
+  }
 }
